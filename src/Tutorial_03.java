dc0592ad29ed465e157f771762bcfc409b0956c6
YourUsernamekavi1414 <kavishkajayathilake04@gmail.com>
2025-08-31 13:03:37 +0530

    Fixed the errors in Tutorial_03.java

--- src/Tutorial_03.java
+++ src/Tutorial_03.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Tutorial_03 {
+    public static void main(String[] args){ //Not add , between String[] and args
+        String str1 = "Hello I am Kavishka";
+        String Arr [] = str1.split(" ");//split() is wrong split("") is correct()
+        for(int i = Arr.length - 1 ; i>=0; i--){
+            System.out.print(Arr[i] + " ");
+        }
+    }
 }
