c1dbd0e89e1a73e6319516b5c2c63af5da737470
YourUsernamekavi1414 <kavishkajayathilake04@gmail.com>
2025-08-31 11:14:18 +0530

    Complete Tutorial_02

--- src/Tutorial_02.java
+++ src/Tutorial_02.java
@@ -1,3 +1,31 @@
 public class Tutorial_02 {
 
+    public static void main (String[] args){
+        int[] arr = {10,0,0,20,30,40};
+        int[] newArr = new int [arr.length] ;
+        rearrange(arr,newArr);
+        print(newArr); //Name is enough not need like (newArr[])
+    }
+
+    private static void rearrange(int arr[], int newArr[]){
+        int j=0;
+      for(int i = 0; i < arr.length; i++){
+          if(arr[i] == 0){  //not = equal is written like ==
+              newArr[j]=arr[i];
+              j++;
+          }
+      }
+
+      for(int i = 0; i <arr.length; i++){
+          arr[j] = arr[i];
+          j++;
+      }
+    }
+
+    public static void print(int[] newArr){
+
+        for( int element : newArr){
+            System.out.print(element + " ");
+        }
+    }
 }
